a4aac12d8a39fc71c0650dab83a284bcf96b0272
Ivan <mvzxw2@gmail.com>
2026-02-23 17:10:20 +0300

    axios fix

--- client/src/shared/api/axiosInstance.ts
+++ client/src/shared/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const instance = axios.create({
-    baseURL: "http://localhost:5000/api/",
+    baseURL: "/api/",
 })
 
 instance.interceptors.request.use((config) => {
