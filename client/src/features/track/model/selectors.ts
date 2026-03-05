427e693b8320eef6b9f9c5f8c5721087fb44db0c
Ivan <mvzxw2@gmail.com>
2026-03-05 14:24:54 +0300

    fix selectors track

--- client/src/features/track/model/selectors.ts
+++ client/src/features/track/model/selectors.ts
@@ -1,8 +1,9 @@
 import { RootState } from "@/app/store";
 
-export const selectTrackList = (state: RootState) => state.track.trackList
-export const selectTrackListStatus = (state: RootState) => state.track.trackListStatus
-export const selectTrackError = (state: RootState) => state.track.error
-
 export const selectTrack = (state: RootState) => state.track.track
 export const selectTrackStatus = (state: RootState) => state.track.trackStatus
+export const selectTrackError = (state: RootState) => state.track.trackError
+
+export const selectTrackList = (state: RootState) => state.track.trackList
+export const selectTrackListStatus = (state: RootState) => state.track.trackListStatus
+export const selectTrackListError = (state: RootState) => state.track.trackListError
