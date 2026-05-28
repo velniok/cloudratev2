7291671dab0f5b6af919db2548b4b41fd05f6f11
Ivan <mvzxw2@gmail.com>
2026-05-28 15:07:11 +0300

    fix mail

--- server/src/services/MailServices.js
+++ server/src/services/MailServices.js
@@ -8,9 +8,9 @@ class MailService {
 
     constructor() {
         this.transporter = nodemailer.createTransport({
-            service: 'gmail',
-            port: 587,
-            secure: false,
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
